f581fb611f8477bf697a94eaae47945a8e2a2ab1
Kevin Caballas <kevincaballas@gmail.com>
2019-02-14 16:48:21 +0800

    Remove viewing of item details within the same screen as to do list

--- components/toDoList/components/todoListItem/toDoListItemComponent.js
+++ components/toDoList/components/todoListItem/toDoListItemComponent.js
@@ -1,41 +1,29 @@
 import React, { Component } from 'react';
 import { Text, View, TouchableOpacity } from 'react-native';
+import { withNavigation } from 'react-navigation';
 
 import styles from './styles';
 
-export default class ToDoListItemComponent extends Component {
+class ToDoListItemComponent extends Component {
   constructor(props) {
     super(props);
-    this.handlePress = this.handlePress.bind(this);
-  }
-
-  handlePress() {
-    if(this.props.id === this.props.detailedView) {
-      this.props.viewDetailsHandler(null);
-    }
-    else {
-      this.props.viewDetailsHandler(this.props.id);
-    }
   }
 
   render() {
-    var details = (this.props.id === this.props.detailedView) ? 
-      <View>
-        <Text>Date added: {this.props.item.dateAdded}</Text>
-        <Text>Description: {this.props.item.description}</Text>
-      </View> :
-      null;
-
     return (
       <TouchableOpacity
         style={styles.itemContainer}
-        onPress={this.handlePress}
+        onPress={() => this.props.navigation.navigate('DetailsScreen', {
+          item: this.props.item
+        })}
       >
         <Text
           style={styles.title}
         >{this.props.item.title}</Text>
-        {details}
+        <Text>Date added: {this.props.item.dateAdded}</Text>
       </TouchableOpacity>
     );
   }
 }
+
+export default withNavigation(ToDoListItemComponent);
--- components/toDoList/toDoListComponent.js
+++ components/toDoList/toDoListComponent.js
@@ -1,41 +1,20 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView } from 'react-native';
+import { Text, View, FlatList } from 'react-native';
 
 import styles from './styles';
 
 import ToDoListItemComponent from './components/todoListItem/toDoListItemComponent';
 
 export default class ToDoListComponent extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      detailedView: null
-    }
-
-    this.viewDetailsHandler = this.viewDetailsHandler.bind(this);
-  }
-
-  viewDetailsHandler(id) {
-    this.setState({
-      ...this.state,
-      detailedView: id
-    });
-  }
-
   render() {
-    const listItems = this.props.toDoList.map((item, index) => {
-      return (
-        <ToDoListItemComponent viewDetailsHandler={this.viewDetailsHandler} detailedView={this.state.detailedView} key={index} id={index} item={item} />
-      );
-    });
-
     return (
       <View style={styles.scrollViewContainer}>
         {(this.props.toDoList.length > 0) ?
-          <ScrollView style={styles.scrollView}>
-            {listItems}
-          </ScrollView>
+          <FlatList
+            data={this.props.toDoList}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({ item, index }) => ( <ToDoListItemComponent viewDetailsHandler={this.viewDetailsHandler} item={item} id={index} /> )}
+          />
           :
           <Text style={styles.emptyMessage}>Nothing to do.</Text>
         }
